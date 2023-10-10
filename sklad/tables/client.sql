6eeefdc643e7bfdaaf3e7a1e810581d1768287b5
Overloadq <i.koshelev000@gmail.com>
2023-10-10 18:02:56 +0300

    clean repo

--- sklad/tables/client.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS sklad.client
-(
-    id         INTEGER     NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    phone      VARCHAR(11) NOT NULL,
-    CONSTRAINT PK_Client PRIMARY KEY (id)
-);
--- sklad/tables/product.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS sklad.product
-(
-    id    INTEGER        NOT NULL,
-    name  VARCHAR(30)    NOT NULL,
-    price NUMERIC(10, 2) NOT NULL CHECK (price > 0),
-    CONSTRAINT PK_Product PRIMARY KEY (id),
-    CONSTRAINT ch_price CHECK (price > 0)
-);
