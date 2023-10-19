3f58a0cbc7c293242d5e85dc10f0bd634d9626d1
Overloadq <i.koshelev000@gmail.com>
2023-10-19 19:49:55 +0300

    fix namings

---
+++ dictionary/functions/Ingredient_ins.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION dictionary.ingredient_ins(_data json) RETURNS json
+    SECURITY DEFINER
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    _name              VARCHAR(100);
+    _ingredienttype_id INTEGER;
+BEGIN
+    SELECT s.name,
+           s.ingredienttype_id
+    INTO _name,
+         _ingredienttype_id
+    FROM JSON_TO_RECORDSET(_data) AS s (name              VARCHAR(100),
+                                        ingredienttype_id INTEGER);
+
+    IF EXISTS(SELECT 1
+              FROM dictionary.ingredient i
+              WHERE i.name = _name)
+    THEN
+        RETURN public.errmessage('dictionary.ingredient_upd.duplicate', 'Такая запись уже существует!', '');
+    END IF;
+
+    INSERT INTO dictionary.ingredient AS ins (name,
+                                              ingredienttype_id)
+    SELECT _name,
+           _ingredienttype_id;
+    RETURN JSON_BUILD_OBJECT('data', NULL);
+END
+$$;
